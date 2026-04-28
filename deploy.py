ef0c49431f44b61c8ccdd38191911b2ce2548122
James Holloway <james.holloway94@gmail.com>
2026-04-28 10:43:29 +0000

    chart ssh connection attempt sync in metadata

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-04-28 09:38:34 UTC
+# Last sync: 2026-04-28 10:43:29 UTC
