c1059c33b4fe57e62295d166eca20f80deeeae84
James Holloway <james.holloway94@gmail.com>
2026-04-29 22:40:23 +0000

    set shield protection sync stamp in config

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-04-29 21:40:58 UTC
+# Last sync: 2026-04-29 22:40:23 UTC
