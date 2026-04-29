aeb52af535c657c289a8135198af76de6e4a5b6f
James Holloway <james.holloway94@gmail.com>
2026-04-29 01:54:25 +0000

    rotate kms key metadata sync after policy apply

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-04-29 00:53:33 UTC
+# Last sync: 2026-04-29 01:54:25 UTC
