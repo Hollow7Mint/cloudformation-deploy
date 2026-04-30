f369b3f1abbad44b397bc00581664030550b6186
James Holloway <james.holloway94@gmail.com>
2026-04-30 09:05:37 +0000

    snapshot iam role binding time for audit trail

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-04-30 07:27:08 UTC
+# Last sync: 2026-04-30 09:05:37 UTC
