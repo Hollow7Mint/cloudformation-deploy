8deeb9a69fb7d03f006254c9e6e4b889733ccb7c
James Holloway <james.holloway94@gmail.com>
2026-04-30 00:47:59 +0000

    post postgres replication heartbeat to config

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-04-29 23:42:53 UTC
+# Last sync: 2026-04-30 00:47:59 UTC
