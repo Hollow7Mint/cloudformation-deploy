21118b24873a665615e15e4ecb1cc1da14084ef8
James Holloway <james.holloway94@gmail.com>
2026-04-30 05:34:58 +0000

    pin route53 dns sync stamp after record update

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-04-30 04:45:48 UTC
+# Last sync: 2026-04-30 05:34:58 UTC
