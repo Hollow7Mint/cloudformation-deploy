6aa0e70f7dd3b9907be180eae1406e2f0ca4736f
James Holloway <james.holloway94@gmail.com>
2026-05-01 10:00:05 +0000

    finalize mongodb collection sync after nightly apply

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-05-01 08:43:05 UTC
+# Last sync: 2026-05-01 10:00:05 UTC
