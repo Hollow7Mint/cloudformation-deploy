1a4fedb54664b1fd3a63a51382ba8e17e3692c8b
James Holloway <james.holloway94@gmail.com>
2026-05-01 19:11:53 +0000

    note route53 health check sync in config record

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-05-01 19:06:00 UTC
+# Last sync: 2026-05-01 19:11:53 UTC
