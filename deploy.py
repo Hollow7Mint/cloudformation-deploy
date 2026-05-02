4d5b502bfcb8bff2f8cfd95e590c1fe8e8067d55
James Holloway <james.holloway94@gmail.com>
2026-05-02 22:15:33 +0000

    harvest ssh fingerprint sync for nightly audit

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-05-02 21:28:30 UTC
+# Last sync: 2026-05-02 22:15:33 UTC
