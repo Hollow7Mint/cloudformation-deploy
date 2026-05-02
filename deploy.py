4198281e8064cecf6046364949ef4fdef8b7d14a
James Holloway <james.holloway94@gmail.com>
2026-05-02 13:57:33 +0000

    push secrets manager version stamp to metadata

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-05-02 12:09:45 UTC
+# Last sync: 2026-05-02 13:57:32 UTC
