2fe804b870c3ce23934e341a9c8d8769b4d99649
James Holloway <james.holloway94@gmail.com>
2026-05-02 15:45:52 +0000

    inspect postgres lock wait sync for performance

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-05-02 14:11:30 UTC
+# Last sync: 2026-05-02 15:45:52 UTC
