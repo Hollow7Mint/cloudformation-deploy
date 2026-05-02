15b19162bda393ed5aa435a379ffb97c0e1b8389
James Holloway <james.holloway94@gmail.com>
2026-05-02 19:43:19 +0000

    bridge postgres foreign key sync through config

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-05-02 18:50:12 UTC
+# Last sync: 2026-05-02 19:43:19 UTC
