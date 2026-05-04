e63c76f94eaedc1f324be21d9c188bca66e60056
James Holloway <james.holloway94@gmail.com>
2026-05-04 23:57:34 +0000

    restamp iam permission boundary sync after review

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-05-04 22:42:59 UTC
+# Last sync: 2026-05-04 23:57:34 UTC
