fa1d497de1f19b2706f24b33b0b740da05652bf5
James Holloway <james.holloway94@gmail.com>
2026-05-04 11:16:18 +0000

    capture db backup window completion time in record

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-05-04 10:11:19 UTC
+# Last sync: 2026-05-04 11:16:18 UTC
