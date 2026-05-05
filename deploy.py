298cb815f90826186edfa2112777337e865d1e3d
James Holloway <james.holloway94@gmail.com>
2026-05-05 18:21:24 +0000

    post ssh key rotation heartbeat to config

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-05-05 17:42:36 UTC
+# Last sync: 2026-05-05 18:21:24 UTC
