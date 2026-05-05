336e21d131367b774ca134a79aba5b9ae144e4ff
James Holloway <james.holloway94@gmail.com>
2026-05-05 00:30:12 +0000

    resolve waf rule group sync into metadata

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-05-04 23:57:34 UTC
+# Last sync: 2026-05-05 00:30:12 UTC
