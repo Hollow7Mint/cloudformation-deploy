8f5fc68c0cfef2ca85a5ed96c81c66e8687b0e6a
James Holloway <james.holloway94@gmail.com>
2026-05-05 01:16:28 +0000

    advance cdk app deploy time into config

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-05-05 00:30:12 UTC
+# Last sync: 2026-05-05 01:16:28 UTC
