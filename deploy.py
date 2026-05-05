2543e172d4716220637fe3a0a23b9437b1065492
James Holloway <james.holloway94@gmail.com>
2026-05-05 11:09:04 +0000

    rebuild nat gateway sync record after route change

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-05-05 10:49:15 UTC
+# Last sync: 2026-05-05 11:09:04 UTC
