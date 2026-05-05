67dbada674050c57bd7236cece8102da5676d8ca
James Holloway <james.holloway94@gmail.com>
2026-05-05 02:21:52 +0000

    note postgres extension sync in config record

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-05-05 01:16:28 UTC
+# Last sync: 2026-05-05 02:21:52 UTC
