2f326522de91f19424bbbca268396c521b98a0b5
James Holloway <james.holloway94@gmail.com>
2026-05-06 04:07:44 +0000

    submit mongodb backup marker after maintenance

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-05-06 02:08:42 UTC
+# Last sync: 2026-05-06 04:07:44 UTC
