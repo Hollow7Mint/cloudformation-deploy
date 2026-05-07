1156d5373c152ef91252ebc656f473f4677bf0f5
James Holloway <james.holloway94@gmail.com>
2026-05-07 23:45:46 +0000

    run ssh certificate heartbeat for scheduled audit

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-05-07 22:08:43 UTC
+# Last sync: 2026-05-07 23:45:46 UTC
