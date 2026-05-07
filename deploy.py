b58dbf7107f8bde1667f37fa29f5a25216f9fcad
James Holloway <james.holloway94@gmail.com>
2026-05-07 08:12:46 +0000

    audit vpc peering sync record after route update

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-05-07 07:33:45 UTC
+# Last sync: 2026-05-07 08:12:46 UTC
