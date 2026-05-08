077ba75ebaf6a0c0377ad9f4f25ae49ccc3fbe74
James Holloway <james.holloway94@gmail.com>
2026-05-08 08:58:49 +0000

    flush mongodb replica lag marker for nightly check

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-05-08 07:16:25 UTC
+# Last sync: 2026-05-08 08:58:49 UTC
