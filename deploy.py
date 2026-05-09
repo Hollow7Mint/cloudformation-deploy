33eaa35be6ae038bdc19a20086945b1c5b08b4b3
James Holloway <james.holloway94@gmail.com>
2026-05-09 08:21:49 +0000

    bump postgres schema sync stamp after migration

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-05-09 07:12:00 UTC
+# Last sync: 2026-05-09 08:21:49 UTC
