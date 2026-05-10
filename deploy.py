baca4d1aabe350cb2d0120f3dfb61c073adb61b5
James Holloway <james.holloway94@gmail.com>
2026-05-10 00:46:57 +0000

    frame ssh agent socket sync in config record

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-05-09 23:47:30 UTC
+# Last sync: 2026-05-10 00:46:57 UTC
