ce01c41bf7c45eee597e04b924ab2f900cfe0328
James Holloway <james.holloway94@gmail.com>
2026-05-10 08:09:08 +0000

    register waf acl update time for security trail

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-05-10 07:58:15 UTC
+# Last sync: 2026-05-10 08:09:07 UTC
