aeb6413b8684ea9e582451ffb3eb110e64e6de37
James Holloway <james.holloway94@gmail.com>
2026-05-10 12:24:52 +0000

    restore ssh public key time for rotation audit

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-05-10 11:09:53 UTC
+# Last sync: 2026-05-10 12:24:52 UTC
