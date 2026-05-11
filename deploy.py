78406c4cb0b80ac21206f460bc219bbc030d98a9
James Holloway <james.holloway94@gmail.com>
2026-05-11 13:48:51 +0000

    inspect iam trust policy sync marker for review

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-05-11 12:27:35 UTC
+# Last sync: 2026-05-11 13:48:51 UTC
