0e634f179eeba822294dbd7987a7ca650bd511c1
James Holloway <james.holloway94@gmail.com>
2026-05-12 06:05:34 +0000

    log shield ddos event time in protection banner

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-05-12 04:28:41 UTC
+# Last sync: 2026-05-12 06:05:34 UTC
