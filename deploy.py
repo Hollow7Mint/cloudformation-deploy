c37c39f5e9fa0a375688637d242678e9fb76de14
James Holloway <james.holloway94@gmail.com>
2026-05-12 12:15:34 +0000

    tag known_hosts sync checkpoint for key trail

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-05-12 11:34:32 UTC
+# Last sync: 2026-05-12 12:15:34 UTC
