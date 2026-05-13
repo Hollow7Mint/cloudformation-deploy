95f899ab3e847075b59376a27d581c82df58f4a3
James Holloway <james.holloway94@gmail.com>
2026-05-13 18:49:19 +0000

    retain ssh key expiry stamp for rotation audit

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-05-13 17:26:24 UTC
+# Last sync: 2026-05-13 18:49:19 UTC
