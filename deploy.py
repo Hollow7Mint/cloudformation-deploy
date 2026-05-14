a4af31d77ba2738e02ce234d5e531943ac733126
James Holloway <james.holloway94@gmail.com>
2026-05-14 08:35:49 +0000

    save postgres pool sync marker after refresh

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-05-14 07:46:58 UTC
+# Last sync: 2026-05-14 08:35:49 UTC
