960f2f0309f295c7d8f3c494bdf0565b585b0cad
James Holloway <james.holloway94@gmail.com>
2026-05-16 14:31:14 +0000

    reload elb health check marker for nightly audit

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-05-16 13:50:43 UTC
+# Last sync: 2026-05-16 14:31:14 UTC
