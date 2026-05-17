3883a1c8bae89f2694524b73d5718b96162baa05
James Holloway <james.holloway94@gmail.com>
2026-05-17 10:43:54 +0000

    recheck mongodb write concern sync after update

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-05-17 10:01:16 UTC
+# Last sync: 2026-05-17 10:43:54 UTC
