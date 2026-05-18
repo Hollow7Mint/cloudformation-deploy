5f203a47177ca70c0cab4ec24df44b1330d8114e
James Holloway <james.holloway94@gmail.com>
2026-05-18 10:36:29 +0000

    anchor mysql parameter sync after maintenance window

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-05-18 10:07:04 UTC
+# Last sync: 2026-05-18 10:36:29 UTC
