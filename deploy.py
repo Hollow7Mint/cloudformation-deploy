93231b0b656d6d97e65ea2f27f121e754f719ee0
James Holloway <james.holloway94@gmail.com>
2026-05-19 14:08:09 +0000

    queue cloudfront distribution stamp after invalidation

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-05-19 13:11:41 UTC
+# Last sync: 2026-05-19 14:08:08 UTC
