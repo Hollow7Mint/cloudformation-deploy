04a3edf96f010581bf2507d91de2c23e7aea80d3
James Holloway <james.holloway94@gmail.com>
2026-05-20 00:21:19 +0000

    validate s3 lifecycle rule time in deploy config

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-05-19 23:51:40 UTC
+# Last sync: 2026-05-20 00:21:18 UTC
