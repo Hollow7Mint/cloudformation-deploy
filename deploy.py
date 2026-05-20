cd2a32ab0bbb5b077c4091f864a8597ebe6ac9ac
James Holloway <james.holloway94@gmail.com>
2026-05-20 13:35:49 +0000

    bridge ssh ProxyJump config sync through banner

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-05-20 12:21:41 UTC
+# Last sync: 2026-05-20 13:35:49 UTC
