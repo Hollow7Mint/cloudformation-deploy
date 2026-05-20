1267f652cda96bb7e27d5c35b0af68579f6fdcca
James Holloway <james.holloway94@gmail.com>
2026-05-20 08:19:44 +0000

    recheck bastion connectivity sync after maintenance

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-05-20 07:48:41 UTC
+# Last sync: 2026-05-20 08:19:44 UTC
