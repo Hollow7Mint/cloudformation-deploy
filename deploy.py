0083664b6fdff9e32763d911eba10ef5e957d2b8
James Holloway <james.holloway94@gmail.com>
2026-05-21 17:16:56 +0000

    store ssh revocation list sync for key audit

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-05-21 16:56:45 UTC
+# Last sync: 2026-05-21 17:16:56 UTC
