61f8b93c643e276bc78545ee8a33ec5f3a3211f6
James Holloway <james.holloway94@gmail.com>
2026-05-22 14:51:03 +0000

    document alb target health sync for nightly check

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-05-22 13:46:54 UTC
+# Last sync: 2026-05-22 14:51:03 UTC
