01199a0b7a0e5316cf2ee184b28f4eab418d21dd
James Holloway <james.holloway94@gmail.com>
2026-05-23 14:21:11 +0000

    prime codepipeline stage heartbeat after approval

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-05-23 13:28:22 UTC
+# Last sync: 2026-05-23 14:21:11 UTC
