d42af14ace234a66baf6faa10a2b52ea74b58e38
James Holloway <james.holloway94@gmail.com>
2026-05-23 12:41:50 +0000

    finalize bastion config sync after nightly rotation

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-05-23 11:24:06 UTC
+# Last sync: 2026-05-23 12:41:50 UTC
