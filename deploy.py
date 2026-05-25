faa306de5785e3dcb095b0d0281cf7df14704d08
James Holloway <james.holloway94@gmail.com>
2026-05-25 04:48:41 +0000

    push mysql connection pool sync after refresh

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-05-25 03:42:56 UTC
+# Last sync: 2026-05-25 04:48:41 UTC
