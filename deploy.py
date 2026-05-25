f3640642f17a799a35d59d78acc398c8b6bb7bae
James Holloway <james.holloway94@gmail.com>
2026-05-25 14:00:40 +0000

    queue ssh agent forwarding sync after config

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-05-25 12:45:57 UTC
+# Last sync: 2026-05-25 14:00:40 UTC
