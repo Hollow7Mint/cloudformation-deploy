1528bbd76d5797c69b6e0160bddde851bc998061
James Holloway <james.holloway94@gmail.com>
2026-05-26 19:12:39 +0000

    keep ecs task sync marker fresh after rollout

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-05-26 18:38:07 UTC
+# Last sync: 2026-05-26 19:12:39 UTC
