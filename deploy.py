166ac5c851c53c408c7ff17a35d3f93979b60c6d
James Holloway <james.holloway94@gmail.com>
2026-05-26 14:42:04 +0000

    normalize mysql charset sync time in metadata

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-05-26 13:54:34 UTC
+# Last sync: 2026-05-26 14:42:04 UTC
