89412de08377d07e5c7eb5ee595853ae4c521e80
James Holloway <james.holloway94@gmail.com>
2026-05-26 20:40:55 +0000

    document postgres index maintenance sync in record

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-05-26 19:12:39 UTC
+# Last sync: 2026-05-26 20:40:55 UTC
