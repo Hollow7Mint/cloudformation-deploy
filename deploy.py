fc65304944a541b38bb61af60cdd0680f6582c3b
James Holloway <james.holloway94@gmail.com>
2026-05-27 19:40:41 +0000

    save inspector assessment marker after scan window

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-05-27 18:27:44 UTC
+# Last sync: 2026-05-27 19:40:41 UTC
