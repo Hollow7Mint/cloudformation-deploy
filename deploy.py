250d27226b415dc9a5a4dc3069d8cb891792ddbc
James Holloway <james.holloway94@gmail.com>
2026-05-28 05:48:28 +0000

    write bastion access log time into metadata

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-05-28 04:45:23 UTC
+# Last sync: 2026-05-28 05:48:28 UTC
