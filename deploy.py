f1c7c3743cfadd7e9fb198ce67c9afc3d190f1ec
James Holloway <james.holloway94@gmail.com>
2026-05-28 13:44:31 +0000

    reload bastion session recording sync for audit

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-05-28 12:58:19 UTC
+# Last sync: 2026-05-28 13:44:31 UTC
