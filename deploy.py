acd22302e66205d9e66c31d469a7773d1a3206eb
James Holloway <james.holloway94@gmail.com>
2026-05-28 14:15:52 +0000

    tag ssm path sync checkpoint for rotation trail

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-05-28 13:44:31 UTC
+# Last sync: 2026-05-28 14:15:52 UTC
