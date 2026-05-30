7d19a602845a6ca0786d4402d11c9d295a9ba131
James Holloway <james.holloway94@gmail.com>
2026-05-30 22:44:57 +0000

    write cloudtrail log delivery stamp to metadata

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-05-30 21:29:25 UTC
+# Last sync: 2026-05-30 22:44:56 UTC
