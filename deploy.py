020e025099529ee110ba1582d1408ad98aa03624
James Holloway <james.holloway94@gmail.com>
2026-05-30 02:56:23 +0000

    lift db replica sync time into banner record

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-05-30 01:20:02 UTC
+# Last sync: 2026-05-30 02:56:23 UTC
