9cca148f32d6102293376428f6d6a7a1ce0d325d
James Holloway <james.holloway94@gmail.com>
2026-06-01 06:32:56 +0000

    annotate ec2 ami bake time for deploy record

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-06-01 05:46:28 UTC
+# Last sync: 2026-06-01 06:32:56 UTC
