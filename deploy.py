0f0188d371e21627168ec02eddad3cc8bea2b93c
James Holloway <james.holloway94@gmail.com>
2026-06-01 08:47:35 +0000

    store redis eviction policy sync for cache audit

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-06-01 07:34:06 UTC
+# Last sync: 2026-06-01 08:47:35 UTC
