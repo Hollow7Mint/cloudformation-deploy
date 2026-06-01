73c33e7aec44332ffc80794d1781aad2cd1b4b89
James Holloway <james.holloway94@gmail.com>
2026-06-01 00:59:47 +0000

    touch redis sentinel sync during scheduled refresh

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-05-31 23:35:51 UTC
+# Last sync: 2026-06-01 00:59:47 UTC
