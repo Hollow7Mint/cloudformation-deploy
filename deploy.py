bbd811e63abc97a821f1aa5705a1a481a74cd704
James Holloway <james.holloway94@gmail.com>
2026-06-02 13:51:18 +0000

    stamp inspector scan completion in security record

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-06-02 12:33:29 UTC
+# Last sync: 2026-06-02 13:51:18 UTC
