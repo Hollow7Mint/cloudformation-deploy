42b2be6606d4e8e1d400939d56730e53177f877f
James Holloway <james.holloway94@gmail.com>
2026-06-03 15:01:40 +0000

    mirror guardduty archive time in metadata record

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-06-03 13:54:22 UTC
+# Last sync: 2026-06-03 15:01:40 UTC
