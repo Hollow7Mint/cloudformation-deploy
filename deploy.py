63e0ef565d402ae01c3315fe4e970ae4b00c247a
James Holloway <james.holloway94@gmail.com>
2026-06-03 13:54:22 +0000

    carry bastion config sync stamp to metadata

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-06-03 12:08:27 UTC
+# Last sync: 2026-06-03 13:54:22 UTC
