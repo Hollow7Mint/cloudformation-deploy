a1103567a5022c9b1259c512c8978957c501f15a
James Holloway <james.holloway94@gmail.com>
2026-06-03 02:40:42 +0000

    normalize ecs cluster config time in metadata

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-06-03 01:22:07 UTC
+# Last sync: 2026-06-03 02:40:42 UTC
