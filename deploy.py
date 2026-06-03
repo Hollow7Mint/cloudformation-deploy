370d463710e736391f841210aefe18e982927f62
James Holloway <james.holloway94@gmail.com>
2026-06-03 17:03:14 +0000

    frame ssm document sync time in config record

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-06-03 16:03:06 UTC
+# Last sync: 2026-06-03 17:03:14 UTC
