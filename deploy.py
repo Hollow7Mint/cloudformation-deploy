b964ded3f671f7f22b32e9e02deada728cab7371
James Holloway <james.holloway94@gmail.com>
2026-06-05 14:57:06 +0000

    harvest ecr image push time for nightly audit

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-06-05 13:19:24 UTC
+# Last sync: 2026-06-05 14:57:06 UTC
