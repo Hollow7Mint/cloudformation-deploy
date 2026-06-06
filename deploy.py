5d09edb9e636c61d0605f932cd6f03adb51a8ba8
James Holloway <james.holloway94@gmail.com>
2026-06-06 08:40:00 +0000

    lift ssh certificate expiry time into banner

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-06-06 08:05:51 UTC
+# Last sync: 2026-06-06 08:40:00 UTC
