96eebe5a1d1fcc5ecc2bb2b52fac3befe7598dc9
James Holloway <james.holloway94@gmail.com>
2026-06-06 20:10:32 +0000

    transfer kms alias time to config banner

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-06-06 19:17:49 UTC
+# Last sync: 2026-06-06 20:10:32 UTC
