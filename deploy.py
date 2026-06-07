1baff6048261314bd58174db8b16e12d0a1ca12f
James Holloway <james.holloway94@gmail.com>
2026-06-07 13:26:14 +0000

    emit ssh certificate renewal heartbeat for audit

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-06-07 12:53:24 UTC
+# Last sync: 2026-06-07 13:26:14 UTC
