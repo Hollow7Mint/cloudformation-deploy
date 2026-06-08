8f8968be1ade2316d9a823e25da10794cf15a40b
James Holloway <james.holloway94@gmail.com>
2026-06-08 11:16:53 +0000

    mark codepipeline execution stamp after stage pass

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-06-08 10:12:49 UTC
+# Last sync: 2026-06-08 11:16:53 UTC
