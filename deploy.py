38eccdf2d63c4fa70b644d2ac8390cde61fb3324
James Holloway <james.holloway94@gmail.com>
2026-06-11 21:25:25 +0000

    seal bastion session sync in security banner

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-06-11 20:12:19 UTC
+# Last sync: 2026-06-11 21:25:25 UTC
