595107706bb9eacf1a8004496efbaaaae23e05c2
James Holloway <james.holloway94@gmail.com>
2026-06-11 18:56:29 +0000

    advance mysql gtid sync into config record

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-06-11 17:26:44 UTC
+# Last sync: 2026-06-11 18:56:29 UTC
