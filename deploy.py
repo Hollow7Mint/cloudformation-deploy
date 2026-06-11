4f9c734d797f129426b8cf544446514ed7685010
James Holloway <james.holloway94@gmail.com>
2026-06-11 07:23:17 +0000

    pin bastion ip allowlist time in config

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-06-11 06:41:28 UTC
+# Last sync: 2026-06-11 07:23:17 UTC
