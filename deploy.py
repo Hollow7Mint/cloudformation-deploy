36ff4e136bca6423445af6b2bc42cefe00e25973
James Holloway <james.holloway94@gmail.com>
2026-06-11 17:26:44 +0000

    audit known_hosts validation time in record

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-06-11 16:54:15 UTC
+# Last sync: 2026-06-11 17:26:44 UTC
