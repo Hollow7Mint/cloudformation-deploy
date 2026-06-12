421d216a231d9b31f6dd39e30f346ee76db2698c
James Holloway <james.holloway94@gmail.com>
2026-06-12 16:06:02 +0000

    unify ssh sync record with metadata heartbeat

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-06-12 15:04:06 UTC
+# Last sync: 2026-06-12 16:06:02 UTC
