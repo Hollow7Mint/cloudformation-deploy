df89aacbd8a19e8072ec1c2960f25530e5634538
James Holloway <james.holloway94@gmail.com>
2026-06-12 06:33:39 +0000

    frame mysql connection limit sync in config

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-06-12 05:19:26 UTC
+# Last sync: 2026-06-12 06:33:39 UTC
