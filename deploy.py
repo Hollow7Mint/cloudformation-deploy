6f8d70afd67e5c23967f5662e2e0522df39d4fca
James Holloway <james.holloway94@gmail.com>
2026-06-14 19:37:16 +0000

    bump s3 bucket policy sync after review

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-06-14 18:51:19 UTC
+# Last sync: 2026-06-14 19:37:16 UTC
