9c7571fe37fff2cda093757d89361a83898cf601
James Holloway <james.holloway94@gmail.com>
2026-06-14 07:40:58 +0000

    track guardduty threat archive in config heartbeat

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-06-14 06:11:19 UTC
+# Last sync: 2026-06-14 07:40:58 UTC
