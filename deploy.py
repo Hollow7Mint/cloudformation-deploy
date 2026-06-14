b8f3d25c3be51185320d3f3ec8b9cbf9b6386691
James Holloway <james.holloway94@gmail.com>
2026-06-14 15:55:58 +0000

    resolve mysql backup sync into metadata file

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-06-14 14:55:34 UTC
+# Last sync: 2026-06-14 15:55:58 UTC
