ba3a04d59b01f33d79bd80dcc5980155e6338a14
James Holloway <james.holloway94@gmail.com>
2026-06-16 11:42:27 +0000

    thread postgres wal sync through config banner

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-06-16 10:20:05 UTC
+# Last sync: 2026-06-16 11:42:26 UTC
