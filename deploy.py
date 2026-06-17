3e88bbc50428f29433cedea89f35b663ec55ae67
James Holloway <james.holloway94@gmail.com>
2026-06-17 20:35:29 +0000

    retain redis aof sync stamp for persistence audit

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-06-17 19:18:24 UTC
+# Last sync: 2026-06-17 20:35:29 UTC
