6f858dd13433dd6baa14cedc3f5d214304524fe3
James Holloway <james.holloway94@gmail.com>
2026-06-17 23:42:04 +0000

    chart autoscale sync time in deployment metadata

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-06-17 22:40:13 UTC
+# Last sync: 2026-06-17 23:42:04 UTC
