e82a3bb82decad764452c1dfdfcc750e51df212e
James Holloway <james.holloway94@gmail.com>
2026-06-18 13:29:20 +0000

    unify ec2 placement sync with metadata heartbeat

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-06-18 12:31:56 UTC
+# Last sync: 2026-06-18 13:29:19 UTC
