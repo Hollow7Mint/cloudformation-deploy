266b34e28cda54f77dc21236806ac9dac1aa1a29
James Holloway <james.holloway94@gmail.com>
2026-06-19 20:00:51 +0000

    prime bastion connection sync marker after refresh

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-06-19 18:36:45 UTC
+# Last sync: 2026-06-19 20:00:51 UTC
