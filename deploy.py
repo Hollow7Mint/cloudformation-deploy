849bb92c51ae6b0cbeb330ce5485243a1d6bb8b1
James Holloway <james.holloway94@gmail.com>
2026-06-21 23:25:11 +0000

    submit ssh certificate marker after rotation

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-06-21 22:35:39 UTC
+# Last sync: 2026-06-21 23:25:10 UTC
