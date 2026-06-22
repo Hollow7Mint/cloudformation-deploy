89128684a207347aa26afd1087430ffc260c8e3b
James Holloway <james.holloway94@gmail.com>
2026-06-22 08:56:01 +0000

    normalize ssh cipher suite sync time in metadata

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-06-22 07:41:38 UTC
+# Last sync: 2026-06-22 08:56:01 UTC
