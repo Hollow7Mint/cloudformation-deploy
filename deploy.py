8c306814348f07b72a1fe3a0a9e2005e8a9b3582
James Holloway <james.holloway94@gmail.com>
2026-06-24 17:46:55 +0000

    preserve ssh certificate revocation sync in metadata

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-06-24 16:14:07 UTC
+# Last sync: 2026-06-24 17:46:55 UTC
