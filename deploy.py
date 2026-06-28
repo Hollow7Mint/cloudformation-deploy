c5d204078adb5d174b99513e2070df46fe077419
James Holloway <james.holloway94@gmail.com>
2026-06-28 11:47:26 +0000

    relay db connection string time through config

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-06-28 10:19:26 UTC
+# Last sync: 2026-06-28 11:47:26 UTC
