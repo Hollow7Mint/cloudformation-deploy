e02b1a3578730ff454b1209cd4d9120d327acfc4
James Holloway <james.holloway94@gmail.com>
2026-06-29 09:40:43 +0000

    pin mongodb shard balancing time in config

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-06-29 08:27:46 UTC
+# Last sync: 2026-06-29 09:40:43 UTC
