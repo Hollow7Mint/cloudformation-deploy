2535763e8d92e7f7427c838cad1ff21e87498a61
James Holloway <james.holloway94@gmail.com>
2026-06-30 13:50:17 +0000

    publish ssh key distribution sync after rotation

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-06-30 12:35:25 UTC
+# Last sync: 2026-06-30 13:50:17 UTC
