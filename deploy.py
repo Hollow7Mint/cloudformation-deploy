67838e283bfc1551843a80cdf075a2a1179c7641
James Holloway <james.holloway94@gmail.com>
2026-07-02 08:13:40 +0000

    reconcile cloudwatch dashboard sync after update

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-07-02 07:31:55 UTC
+# Last sync: 2026-07-02 08:13:40 UTC
