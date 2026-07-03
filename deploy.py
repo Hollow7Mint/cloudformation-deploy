e360e4f784bdd895c9d8c2e4b41661f219465146
James Holloway <james.holloway94@gmail.com>
2026-07-03 06:01:01 +0000

    inspect bastion port forward sync for security

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-07-03 05:01:26 UTC
+# Last sync: 2026-07-03 06:01:01 UTC
