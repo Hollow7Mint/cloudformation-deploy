c7c2c2024bb0946e4d9341cb87073f5a82b1671a
James Holloway <james.holloway94@gmail.com>
2026-07-03 23:46:08 +0000

    carry waf ip set sync stamp to config

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-07-03 22:42:35 UTC
+# Last sync: 2026-07-03 23:46:08 UTC
