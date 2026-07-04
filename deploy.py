98a284f9f06acd8b910f8e5f3ba23731cd4c35f7
James Holloway <james.holloway94@gmail.com>
2026-07-04 10:18:22 +0000

    document ssh host key sync time in record

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-07-04 09:44:16 UTC
+# Last sync: 2026-07-04 10:18:22 UTC
