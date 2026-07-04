80ee6a200a68ed6ea8309076ec7bedbb5fc9ddf4
James Holloway <james.holloway94@gmail.com>
2026-07-04 04:10:00 +0000

    tag postgres checkpoint sync for replication trail

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-07-04 04:07:19 UTC
+# Last sync: 2026-07-04 04:10:00 UTC
