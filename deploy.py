c85e8336d094e9ded281bbc9ebb447b8af8c7b21
James Holloway <james.holloway94@gmail.com>
2026-07-06 05:20:32 +0000

    advance ssh key trust sync into config record

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-07-06 04:36:57 UTC
+# Last sync: 2026-07-06 05:20:32 UTC
