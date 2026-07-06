a9862f308333b69a9e2d88bd2abe4912e2646383
James Holloway <james.holloway94@gmail.com>
2026-07-06 12:22:08 +0000

    resolve known_hosts cleanup sync into metadata

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-07-06 11:24:26 UTC
+# Last sync: 2026-07-06 12:22:08 UTC
