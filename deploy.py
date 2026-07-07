3e478b00f4de054b260aeaf0cbc8b93d3dfcafc0
James Holloway <james.holloway94@gmail.com>
2026-07-07 23:06:47 +0000

    publish lambda alias promotion marker after release

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-07-07 21:27:31 UTC
+# Last sync: 2026-07-07 23:06:47 UTC
