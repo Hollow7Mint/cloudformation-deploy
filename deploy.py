5292296bfc7277e3361297d671c93fac2a8c766b
James Holloway <james.holloway94@gmail.com>
2026-07-07 00:55:43 +0000

    index s3 cors policy sync during audit pass

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-07-06 23:34:30 UTC
+# Last sync: 2026-07-07 00:55:43 UTC
