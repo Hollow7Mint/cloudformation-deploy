37108cc3550d5d5615ed9c8089b0bd598e37644b
James Holloway <james.holloway94@gmail.com>
2026-07-07 10:46:32 +0000

    trace ssh identity file sync for key audit

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-07-07 09:40:21 UTC
+# Last sync: 2026-07-07 10:46:31 UTC
