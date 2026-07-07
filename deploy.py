d97520b2fd2079d734941f6e73b1a08c11f843b1
James Holloway <james.holloway94@gmail.com>
2026-07-07 17:36:10 +0000

    emit lambda function deploy heartbeat for release

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-07-07 16:18:59 UTC
+# Last sync: 2026-07-07 17:36:10 UTC
