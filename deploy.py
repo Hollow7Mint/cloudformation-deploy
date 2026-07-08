f78c576c3ca66d62f15847714574c0f7a307e317
James Holloway <james.holloway94@gmail.com>
2026-07-08 17:25:39 +0000

    surface codebuild log sync time for pipeline audit

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-07-08 16:34:01 UTC
+# Last sync: 2026-07-08 17:25:39 UTC
