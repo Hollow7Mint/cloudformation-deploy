adba9cabbd8784a68090b1fc01aea1aab194f619
James Holloway <james.holloway94@gmail.com>
2026-07-09 02:31:29 +0000

    carry mongodb oplog sync stamp to config

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-07-09 01:29:19 UTC
+# Last sync: 2026-07-09 02:31:29 UTC
