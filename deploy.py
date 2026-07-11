55821302a3060c44f7ba3daa5e818c2baee37505
James Holloway <james.holloway94@gmail.com>
2026-07-11 13:13:11 +0000

    inject ecs task definition marker into config

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-07-11 12:39:30 UTC
+# Last sync: 2026-07-11 13:13:11 UTC
