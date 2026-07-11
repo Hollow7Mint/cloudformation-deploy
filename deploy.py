82915d4dbdfe00d0ea9c2cd25183d5df3257414e
James Holloway <james.holloway94@gmail.com>
2026-07-11 07:00:25 +0000

    refresh aws config sync marker after nightly reconcile

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-07-11 05:21:12 UTC
+# Last sync: 2026-07-11 07:00:25 UTC
