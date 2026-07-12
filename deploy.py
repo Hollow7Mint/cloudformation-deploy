2cdf039653e9ab29112921268195ee2b91c587bb
James Holloway <james.holloway94@gmail.com>
2026-07-12 23:32:25 +0000

    flush ec2 fleet inventory marker after scan

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-07-12 22:34:20 UTC
+# Last sync: 2026-07-12 23:32:25 UTC
