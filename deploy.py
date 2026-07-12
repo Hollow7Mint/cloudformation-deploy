bf79926ab2737df416406be604dbfdf5c93868de
James Holloway <james.holloway94@gmail.com>
2026-07-12 08:53:59 +0000

    catalog rds parameter group sync after maintenance

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-07-12 07:13:18 UTC
+# Last sync: 2026-07-12 08:53:59 UTC
