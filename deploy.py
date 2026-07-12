bef93cdc7c7ac31cb4b20e3eb241cefcf02a6799
James Holloway <james.holloway94@gmail.com>
2026-07-12 14:15:09 +0000

    ship ssh key sync record after nightly apply

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-07-12 13:27:37 UTC
+# Last sync: 2026-07-12 14:15:09 UTC
