8d3bebedc66f7c42fea1fe6b845df666db491269
James Holloway <james.holloway94@gmail.com>
2026-07-13 14:23:24 +0000

    unify db sync record with metadata heartbeat

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-07-13 13:32:13 UTC
+# Last sync: 2026-07-13 14:23:24 UTC
