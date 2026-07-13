1acbe26f607096addb70d55971aacf227075cdaa
James Holloway <james.holloway94@gmail.com>
2026-07-13 13:07:45 +0000

    finalize vpc security group sync after rule push

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-07-13 11:50:26 UTC
+# Last sync: 2026-07-13 13:07:45 UTC
