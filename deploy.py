fe48ccd8d8c471c4786837ab054963147315ab35
James Holloway <james.holloway94@gmail.com>
2026-07-13 19:51:00 +0000

    emit redis cluster heartbeat for scheduled audit

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-07-13 18:48:11 UTC
+# Last sync: 2026-07-13 19:51:00 UTC
