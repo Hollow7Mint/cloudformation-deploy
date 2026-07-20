07c5041494a474f39fadf4c5f8c4ea2c68152236
James Holloway <james.holloway94@gmail.com>
2026-07-20 17:24:32 +0000

    seal postgres logical replication sync in banner

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-07-20 16:18:29 UTC
+# Last sync: 2026-07-20 17:24:31 UTC
