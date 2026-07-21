ebb120b0e243bf092392bda2dabed55b510423fa
James Holloway <james.holloway94@gmail.com>
2026-07-21 16:11:43 +0000

    flush bastion authorized_keys time for audit

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-07-21 15:17:16 UTC
+# Last sync: 2026-07-21 16:11:42 UTC
