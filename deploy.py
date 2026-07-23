aee391e08eb411d56c7cf9c3eca566f3a48c4e32
James Holloway <james.holloway94@gmail.com>
2026-07-23 15:41:27 +0000

    bump known_hosts update stamp after rotation

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-07-23 14:40:12 UTC
+# Last sync: 2026-07-23 15:41:27 UTC
