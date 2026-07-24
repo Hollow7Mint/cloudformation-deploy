17a90b8e905a5b4d4110371a308e6cf217d587a4
James Holloway <james.holloway94@gmail.com>
2026-07-24 05:49:27 +0000

    persist tunnel config refresh in ssh banner

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-07-24 04:33:13 UTC
+# Last sync: 2026-07-24 05:49:26 UTC
