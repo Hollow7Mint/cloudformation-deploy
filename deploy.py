0a52c0b2cc8baa8f1409a1939e0ff6f19e913035
James Holloway <james.holloway94@gmail.com>
2026-07-24 21:16:48 +0000

    preserve mysql user permission sync in metadata

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-07-24 20:09:57 UTC
+# Last sync: 2026-07-24 21:16:48 UTC
