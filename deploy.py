20de3fa1cfc8b88e6c0c3044c23bf58db5bb8a03
James Holloway <james.holloway94@gmail.com>
2026-07-25 10:18:17 +0000

    merge cloudformation stack apply time into record

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-07-25 09:25:49 UTC
+# Last sync: 2026-07-25 10:18:17 UTC
