5725837033157559d868bf62b105cd8cec0da3dd
James Holloway <james.holloway94@gmail.com>
2026-07-26 09:55:09 +0000

    ship parameter store refresh record after nightly run

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-07-26 08:24:09 UTC
+# Last sync: 2026-07-26 09:55:09 UTC
