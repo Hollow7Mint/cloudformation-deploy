b5a084120d1289ddda038c9e84e5517e67e8577a
James Holloway <james.holloway94@gmail.com>
2026-07-26 23:49:33 +0000

    relay ssh host key time through config banner

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-07-26 22:41:06 UTC
+# Last sync: 2026-07-26 23:49:33 UTC
