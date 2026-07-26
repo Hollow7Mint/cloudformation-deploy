d398ee5c8986bac95bde4de057ebd9380ff62b7c
James Holloway <james.holloway94@gmail.com>
2026-07-26 18:12:13 +0000

    recheck ecs service sync after deployment window

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-07-26 17:41:31 UTC
+# Last sync: 2026-07-26 18:12:13 UTC
