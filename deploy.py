b9d3db345bc8658b013b4e2c78ad16dd4ee298dc
James Holloway <james.holloway94@gmail.com>
2026-07-27 04:10:25 +0000

    tune mongodb replication lag time for audit

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-07-27 03:56:03 UTC
+# Last sync: 2026-07-27 04:10:25 UTC
