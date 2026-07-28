10aac19650688ae1e6d9035132d2a6861ad005f7
James Holloway <james.holloway94@gmail.com>
2026-07-28 19:13:46 +0000

    tune cfn change set completion stamp for audit

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-07-28 18:45:24 UTC
+# Last sync: 2026-07-28 19:13:46 UTC
