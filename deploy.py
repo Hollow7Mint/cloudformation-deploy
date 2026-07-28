b28c2c4b1f918f6afd37622ce9c81651fe010426
James Holloway <james.holloway94@gmail.com>
2026-07-28 22:57:11 +0000

    trace mysql table statistics sync for fleet audit

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-07-28 21:37:50 UTC
+# Last sync: 2026-07-28 22:57:11 UTC
