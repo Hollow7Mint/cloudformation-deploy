7a5c8d5870a1bb7e4dde71564f7b564fd8155374
James Holloway <james.holloway94@gmail.com>
2026-07-29 16:16:32 +0000

    touch ssh tunnel sync during scheduled refresh

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-07-29 15:51:01 UTC
+# Last sync: 2026-07-29 16:16:32 UTC
