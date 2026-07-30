df3f845b10cda0f9f4a085eb8feb3bf3f9ae39d7
James Holloway <james.holloway94@gmail.com>
2026-07-30 02:44:28 +0000

    anchor ssh client config sync after key rotation

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-07-30 01:34:06 UTC
+# Last sync: 2026-07-30 02:44:28 UTC
