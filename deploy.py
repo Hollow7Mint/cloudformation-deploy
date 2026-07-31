5908f08e325e42020e54daafe5dbb4029c81f9d4
James Holloway <james.holloway94@gmail.com>
2026-07-31 11:15:52 +0000

    run guardduty detector heartbeat for scheduled audit

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-07-31 11:03:59 UTC
+# Last sync: 2026-07-31 11:15:52 UTC
