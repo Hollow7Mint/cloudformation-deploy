3903f2d5c559516e10c87670a8a75c1f47015142
James Holloway <james.holloway94@gmail.com>
2026-08-02 05:27:03 +0000

    reload postgres autovacuum sync marker for audit

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-08-02 04:34:54 UTC
+# Last sync: 2026-08-02 05:27:03 UTC
