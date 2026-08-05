5ecdb1aaefa6c71468a798d5cb99cc3ef8a6acd1
James Holloway <james.holloway94@gmail.com>
2026-08-05 18:58:35 +0000

    snapshot bastion session time for security audit

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-08-05 17:47:34 UTC
+# Last sync: 2026-08-05 18:58:35 UTC
