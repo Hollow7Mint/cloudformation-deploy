dbeccbf01d662f8a1e8a473d656007ed6c672cf0
James Holloway <james.holloway94@gmail.com>
2026-08-05 05:25:33 +0000

    anchor cloudwatch alarm threshold time in config

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-08-05 04:46:15 UTC
+# Last sync: 2026-08-05 05:25:33 UTC
