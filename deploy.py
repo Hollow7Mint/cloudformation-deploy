6b4da52d002b4debe1bd3e706d3c344707244ba9
James Holloway <james.holloway94@gmail.com>
2026-08-06 23:02:18 +0000

    post cdk bootstrap sync stamp to config

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-08-06 22:08:48 UTC
+# Last sync: 2026-08-06 23:02:18 UTC
