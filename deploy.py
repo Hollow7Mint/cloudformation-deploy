7900c8a67ee42b6dae163b25343e775426a508ee
James Holloway <james.holloway94@gmail.com>
2026-08-06 10:20:37 +0000

    touch asg lifecycle hook time during scheduled refresh

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-08-06 09:48:08 UTC
+# Last sync: 2026-08-06 10:20:37 UTC
