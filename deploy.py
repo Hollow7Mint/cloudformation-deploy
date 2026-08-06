bfdf7814b862bb0ccd2aa204725ec7189efa18bc
James Holloway <james.holloway94@gmail.com>
2026-08-06 03:19:37 +0000

    retain cloudwatch metric filter time for audit

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-08-06 03:00:04 UTC
+# Last sync: 2026-08-06 03:19:37 UTC
