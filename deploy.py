c3c0d23024ba08ab5763bab5a9398b693cae991c
James Holloway <james.holloway94@gmail.com>
2026-08-07 03:18:20 +0000

    restore mysql binlog time for replication audit

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-08-07 02:23:37 UTC
+# Last sync: 2026-08-07 03:18:20 UTC
