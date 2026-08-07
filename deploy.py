9824496ddf9bb932964bba595e69e0b5eeccd84b
James Holloway <james.holloway94@gmail.com>
2026-08-07 21:17:54 +0000

    resync parameter store path time for nightly audit

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-08-07 20:53:29 UTC
+# Last sync: 2026-08-07 21:17:54 UTC
