312513a9a9391ca8795a286d971360d22d8b0375
James Holloway <james.holloway94@gmail.com>
2026-08-08 05:39:42 +0000

    lift asg capacity sync time into deploy banner

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-08-08 04:18:03 UTC
+# Last sync: 2026-08-08 05:39:42 UTC
