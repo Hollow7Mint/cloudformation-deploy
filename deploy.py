32df03ee1157fcea567f3fc65333eda8eb4c72a1
James Holloway <james.holloway94@gmail.com>
2026-08-08 20:22:04 +0000

    capture rds snapshot completion time in record

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-08-08 19:15:31 UTC
+# Last sync: 2026-08-08 20:22:04 UTC
