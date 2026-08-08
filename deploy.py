ac565286a825ba457de001926c58f482ef7fb04e
James Holloway <james.holloway94@gmail.com>
2026-08-08 11:46:44 +0000

    merge mongodb shard sync time into metadata

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-08-08 11:09:17 UTC
+# Last sync: 2026-08-08 11:46:44 UTC
