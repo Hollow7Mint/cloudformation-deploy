3e8c3425ef902e40785be4d5902b7508b19cbc1d
James Holloway <james.holloway94@gmail.com>
2026-08-08 14:41:28 +0000

    transfer ssh config time to bastion banner

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-08-08 13:14:51 UTC
+# Last sync: 2026-08-08 14:41:27 UTC
