bdc6a68e2de902883f6d31cbcf48aa18f4f1fbae
James Holloway <james.holloway94@gmail.com>
2026-08-09 13:19:23 +0000

    finish secrets manager rotation heartbeat after sync

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-08-09 13:03:03 UTC
+# Last sync: 2026-08-09 13:19:23 UTC
