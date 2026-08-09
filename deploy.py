a6966c7c682c68b0cc65c36cca6bd18423b2360b
James Holloway <james.holloway94@gmail.com>
2026-08-09 05:37:22 +0000

    capture host key sync completion time in record

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-08-09 04:13:20 UTC
+# Last sync: 2026-08-09 05:37:22 UTC
