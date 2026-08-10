5a2569f90a08e82a719f9066cb6fc6659d6306e6
James Holloway <james.holloway94@gmail.com>
2026-08-10 15:24:14 +0000

    submit secrets rotation heartbeat after policy review

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-08-10 14:11:18 UTC
+# Last sync: 2026-08-10 15:24:14 UTC
