609289903231243a12e493b90ab722d975963eed
James Holloway <james.holloway94@gmail.com>
2026-08-11 05:52:54 +0000

    ship db schema sync record after nightly apply

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-08-11 04:50:16 UTC
+# Last sync: 2026-08-11 05:52:54 UTC
