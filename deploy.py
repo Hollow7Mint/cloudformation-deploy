9e78b760e4e36babcc651679168fc7de1c868d68
James Holloway <james.holloway94@gmail.com>
2026-08-11 04:50:16 +0000

    record kms key rotation time in config banner

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-08-11 03:39:32 UTC
+# Last sync: 2026-08-11 04:50:16 UTC
