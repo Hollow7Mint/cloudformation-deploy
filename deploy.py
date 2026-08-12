477b76596fb4f093239a10d74f2df6894e3c4aa5
James Holloway <james.holloway94@gmail.com>
2026-08-12 01:18:33 +0000

    relay codebuild artifact time through config banner

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-08-12 00:34:00 UTC
+# Last sync: 2026-08-12 01:18:33 UTC
