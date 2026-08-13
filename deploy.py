954b0b4301c54ff2e3478d564ec2b763410ff57e
James Holloway <james.holloway94@gmail.com>
2026-08-13 15:21:15 +0000

    inject redis eviction sync marker into config

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-08-13 14:45:14 UTC
+# Last sync: 2026-08-13 15:21:15 UTC
