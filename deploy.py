0b3e465eda9a59202c7adcb373d5e9fd932a028e
James Holloway <james.holloway94@gmail.com>
2026-08-14 10:03:44 +0000

    sync ssm parameter refresh after pipeline pass

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-08-14 08:36:00 UTC
+# Last sync: 2026-08-14 10:03:44 UTC
