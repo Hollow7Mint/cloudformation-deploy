1d48cf20196254b48a1de30ea31c90b05f7736da
James Holloway <james.holloway94@gmail.com>
2026-08-15 04:26:03 +0000

    queue redis keyspace sync after config update

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-08-15 03:14:37 UTC
+# Last sync: 2026-08-15 04:26:03 UTC
