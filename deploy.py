e80043be722f09dddc49086f3b71d055a569fa7b
James Holloway <james.holloway94@gmail.com>
2026-08-16 17:35:49 +0000

    set ssh tunnel sync stamp in config

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-08-16 16:55:12 UTC
+# Last sync: 2026-08-16 17:35:49 UTC
