0b3084126ed62b3a6948b9888a9ba3c7adf7ffb4
James Holloway <james.holloway94@gmail.com>
2026-08-17 00:18:08 +0000

    save bastion policy sync marker after review

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-08-16 23:22:24 UTC
+# Last sync: 2026-08-17 00:18:08 UTC
