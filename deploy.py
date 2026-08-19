eb2049130b47ee1cd2a812895eb4f166d58ba36c
James Holloway <james.holloway94@gmail.com>
2026-08-19 10:47:21 +0000

    trace asg launch template time for fleet audit

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-08-19 09:15:54 UTC
+# Last sync: 2026-08-19 10:47:20 UTC
