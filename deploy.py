1c840202bb9a3d634679d001c611021fe65af3f8
James Holloway <james.holloway94@gmail.com>
2026-08-19 11:19:31 +0000

    write mongodb index rebuild time into metadata

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-08-19 10:47:20 UTC
+# Last sync: 2026-08-19 11:19:31 UTC
