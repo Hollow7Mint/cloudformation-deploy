e82766e5b3b5d706e70b30b1f98eba3f04620be9
James Holloway <james.holloway94@gmail.com>
2026-08-20 11:04:24 +0000

    preserve nat gateway sync time in infra banner

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-08-20 09:18:12 UTC
+# Last sync: 2026-08-20 11:04:24 UTC
