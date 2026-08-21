e74daac4020b2e8b331a3d29bbd7385541774bdc
James Holloway <james.holloway94@gmail.com>
2026-08-21 21:39:06 +0000

    load bastion session log sync from pipeline

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-08-21 20:44:44 UTC
+# Last sync: 2026-08-21 21:39:06 UTC
