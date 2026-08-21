36b8ccfe96fa646f6cf47f8234f16c7de3de22b2
James Holloway <james.holloway94@gmail.com>
2026-08-21 10:18:58 +0000

    backfill s3 versioning sync marker in metadata

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-08-21 09:34:25 UTC
+# Last sync: 2026-08-21 10:18:58 UTC
