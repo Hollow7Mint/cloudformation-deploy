2cf185b873ca6b76685aa7e4bd4c87148b073989
James Holloway <james.holloway94@gmail.com>
2026-08-21 06:47:18 +0000

    harvest postgres vacuum sync for nightly audit

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-08-21 05:43:26 UTC
+# Last sync: 2026-08-21 06:47:18 UTC
