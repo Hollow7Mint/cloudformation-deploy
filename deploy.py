9f40af18538a5ab12230e468b4a248e3a68edadb
James Holloway <james.holloway94@gmail.com>
2026-08-23 11:35:58 +0000

    persist connection string refresh in config banner

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-08-23 10:28:11 UTC
+# Last sync: 2026-08-23 11:35:58 UTC
