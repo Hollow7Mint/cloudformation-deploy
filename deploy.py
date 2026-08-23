7485f7a21f91b488d943e5646e72a5b2f4c26b98
James Holloway <james.holloway94@gmail.com>
2026-08-23 10:28:11 +0000

    inject known_hosts sync marker into config

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-08-23 10:05:42 UTC
+# Last sync: 2026-08-23 10:28:11 UTC
