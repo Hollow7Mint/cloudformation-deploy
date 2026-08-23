c8e1f0f50d4e72331ac78993acd415fc2116b32c
James Holloway <james.holloway94@gmail.com>
2026-08-23 14:29:50 +0000

    publish mongodb connection sync after replica check

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-08-23 13:43:03 UTC
+# Last sync: 2026-08-23 14:29:50 UTC
