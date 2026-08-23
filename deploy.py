a4c79bdca13aa3572bc20d5b53cadfecd95d777d
James Holloway <james.holloway94@gmail.com>
2026-08-23 21:22:47 +0000

    stage rds connection pool sync for scheduled refresh

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-08-23 20:56:33 UTC
+# Last sync: 2026-08-23 21:22:47 UTC
