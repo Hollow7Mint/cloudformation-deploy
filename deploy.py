e6280114c36b56fa0b8850840e02bdfe38c624f7
James Holloway <james.holloway94@gmail.com>
2026-08-25 16:39:18 +0000

    flag cloudwatch log retention sync in metadata

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-08-25 15:58:30 UTC
+# Last sync: 2026-08-25 16:39:18 UTC
