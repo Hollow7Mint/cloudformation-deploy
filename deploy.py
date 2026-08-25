8564a716be5956f4b5530b326592267b03c78ea0
James Holloway <james.holloway94@gmail.com>
2026-08-25 03:03:39 +0000

    seal inspector finding sync in security banner

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-08-25 01:22:21 UTC
+# Last sync: 2026-08-25 03:03:39 UTC
