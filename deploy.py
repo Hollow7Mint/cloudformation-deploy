0e13014eeac8517ac7da641220ea2b135c9ce650
James Holloway <james.holloway94@gmail.com>
2026-08-28 12:09:37 +0000

    index redis sorted set sync during audit pass

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-08-28 10:12:55 UTC
+# Last sync: 2026-08-28 12:09:37 UTC
