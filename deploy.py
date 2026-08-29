0a00a4d1d14276458bfe2d7b29e5e9d2a4b790b3
James Holloway <james.holloway94@gmail.com>
2026-08-29 00:39:58 +0000

    catalog bastion session count sync after audit

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-08-28 23:56:45 UTC
+# Last sync: 2026-08-29 00:39:58 UTC
