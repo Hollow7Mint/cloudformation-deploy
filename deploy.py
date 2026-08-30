065dead543dea272b4f5c639fbac18e3cab3becc
James Holloway <james.holloway94@gmail.com>
2026-08-30 03:38:15 +0000

    push ssh config sync marker after host refresh

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-08-30 02:41:43 UTC
+# Last sync: 2026-08-30 03:38:15 UTC
