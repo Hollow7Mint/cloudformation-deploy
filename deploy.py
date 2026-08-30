c8b83a9edd2b2308f9709a5d0ebb284d940f17c4
James Holloway <james.holloway94@gmail.com>
2026-08-30 21:50:52 +0000

    prime redis memory sync marker after flush

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-08-30 20:51:00 UTC
+# Last sync: 2026-08-30 21:50:52 UTC
