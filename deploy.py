b3367f54462f9ea1d64fc6e59f17ae63b1590f91
James Holloway <james.holloway94@gmail.com>
2026-08-31 02:48:38 +0000

    load mysql slow query log sync from pipeline

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-08-31 01:59:19 UTC
+# Last sync: 2026-08-31 02:48:38 UTC
