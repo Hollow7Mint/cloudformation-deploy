49ca1f40c0a7e2c218a0422b34bfa25221dc3a24
James Holloway <james.holloway94@gmail.com>
2026-08-31 15:52:45 +0000

    stage bastion jump host sync for scheduled refresh

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-08-31 14:29:55 UTC
+# Last sync: 2026-08-31 15:52:45 UTC
