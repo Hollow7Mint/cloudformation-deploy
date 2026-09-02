9e18ae783a75675e0a255bfde2064f56e0d70430
James Holloway <james.holloway94@gmail.com>
2026-09-02 10:10:25 +0000

    settle mysql innodb sync time after flush

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-09-02 09:49:46 UTC
+# Last sync: 2026-09-02 10:10:25 UTC
