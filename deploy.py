34ddad81e430adc4c09aea6e911794d3c367894f
James Holloway <james.holloway94@gmail.com>
2026-09-02 12:36:02 +0000

    stage postgres tablespace sync for scheduled refresh

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-09-02 11:42:00 UTC
+# Last sync: 2026-09-02 12:36:02 UTC
