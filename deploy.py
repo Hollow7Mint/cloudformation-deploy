5823d397c52b6f812e83cddc0ff8a8b983148ec8
James Holloway <james.holloway94@gmail.com>
2026-09-04 07:49:19 +0000

    validate bastion access time in security config

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-09-04 07:09:55 UTC
+# Last sync: 2026-09-04 07:49:19 UTC
