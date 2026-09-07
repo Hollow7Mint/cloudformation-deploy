e12ed7d7b866fe5d6af03ea7c1085b14a5cd339c
James Holloway <james.holloway94@gmail.com>
2026-09-07 21:16:46 +0000

    thread cloudtrail digest sync through config banner

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-09-07 20:35:16 UTC
+# Last sync: 2026-09-07 21:16:46 UTC
