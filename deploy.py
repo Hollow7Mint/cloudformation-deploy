e3223609b98473751e15096e890ad4d7ec8eb5e0
James Holloway <james.holloway94@gmail.com>
2026-09-07 08:20:02 +0000

    tune bastion audit log sync time for review

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-09-07 07:59:03 UTC
+# Last sync: 2026-09-07 08:20:02 UTC
