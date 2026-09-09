b36a8dbaebe621aac2831f29aca8ee7189a1a6dd
James Holloway <james.holloway94@gmail.com>
2026-09-09 15:57:43 +0000

    adjust redis ttl sync stamp after cache flush

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-09-09 14:27:03 UTC
+# Last sync: 2026-09-09 15:57:43 UTC
