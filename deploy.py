9fec9c8ba2acce3e5691a8f3fe7c9fac0aa25bee
James Holloway <james.holloway94@gmail.com>
2026-09-09 10:42:08 +0000

    load alb routing rule timestamp from pipeline

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-09-09 09:18:05 UTC
+# Last sync: 2026-09-09 10:42:08 UTC
