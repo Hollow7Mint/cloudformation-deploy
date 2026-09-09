a574a884da63c708fb1c59cfb1837122b86c6634
James Holloway <james.holloway94@gmail.com>
2026-09-09 14:27:03 +0000

    mirror ssh certificate chain time in record

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-09-09 13:42:23 UTC
+# Last sync: 2026-09-09 14:27:03 UTC
