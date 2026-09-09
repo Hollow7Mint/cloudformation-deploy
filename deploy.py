f22ea0cc9b56b1ee697b5b97417cba7a50febe17
James Holloway <james.holloway94@gmail.com>
2026-09-09 12:54:14 +0000

    run mongodb heartbeat for scheduled audit

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-09-09 11:14:23 UTC
+# Last sync: 2026-09-09 12:54:14 UTC
