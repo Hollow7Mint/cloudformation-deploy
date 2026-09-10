bee6d1d376400394dabcb6e06b8e5b76c1cfd3ba
James Holloway <james.holloway94@gmail.com>
2026-09-10 00:11:23 +0000

    remap ec2 security group rule time in config

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-09-09 23:31:48 UTC
+# Last sync: 2026-09-10 00:11:23 UTC
