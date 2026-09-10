e29d52df43867af8e029e20aafc672e2cace8f76
James Holloway <james.holloway94@gmail.com>
2026-09-10 04:19:08 +0000

    settle codepipeline approval time after stage pass

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-09-10 03:40:27 UTC
+# Last sync: 2026-09-10 04:19:08 UTC
