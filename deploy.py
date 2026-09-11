b4fd5dda98f5b1845ad225f75c4ac1377aadb6bb
James Holloway <james.holloway94@gmail.com>
2026-09-11 01:33:25 +0000

    merge ssh trust anchor time into metadata

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-09-11 00:25:47 UTC
+# Last sync: 2026-09-11 01:33:25 UTC
