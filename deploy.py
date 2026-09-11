827cf86c3675db6416edf1255bb07ba952141a25
James Holloway <james.holloway94@gmail.com>
2026-09-11 14:31:41 +0000

    bridge lambda env var sync through config banner

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-09-11 13:35:16 UTC
+# Last sync: 2026-09-11 14:31:41 UTC
