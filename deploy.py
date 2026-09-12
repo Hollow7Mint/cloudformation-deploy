8d94f6dda38a6f402b3d5d3d19cdce12994e71a1
James Holloway <james.holloway94@gmail.com>
2026-09-12 10:24:57 +0000

    thread bastion log sync through config banner

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-09-12 09:51:36 UTC
+# Last sync: 2026-09-12 10:24:57 UTC
