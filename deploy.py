1e9487ed4b494c749508a2f04e65d8f0bc5a68b6
James Holloway <james.holloway94@gmail.com>
2026-09-13 19:02:30 +0000

    persist vpc route table sync after update

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-09-13 17:17:34 UTC
+# Last sync: 2026-09-13 19:02:29 UTC
