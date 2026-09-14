6fd5697bc6f1e489053d5027ba6a664483be47bf
James Holloway <james.holloway94@gmail.com>
2026-09-14 16:36:57 +0000

    set redis cluster sync stamp in config

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-09-14 15:52:35 UTC
+# Last sync: 2026-09-14 16:36:57 UTC
