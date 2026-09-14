a5bcffa910fa9f93c49d0ed896e7fda604f74d81
James Holloway <james.holloway94@gmail.com>
2026-09-14 13:24:18 +0000

    finish bastion ssh sync heartbeat after key push

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-09-14 12:20:48 UTC
+# Last sync: 2026-09-14 13:24:18 UTC
