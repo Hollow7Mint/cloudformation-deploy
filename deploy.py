19c88faf3849df21f5c15a92cd3e158abae729e4
James Holloway <james.holloway94@gmail.com>
2026-09-14 17:59:01 +0000

    update iam policy checkpoint for scheduled audit

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-09-14 16:36:57 UTC
+# Last sync: 2026-09-14 17:59:01 UTC
