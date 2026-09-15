efd6564fdb16cfe58e4836fb67aacedfc403a4c1
James Holloway <james.holloway94@gmail.com>
2026-09-15 12:26:27 +0000

    adjust bastion acl sync stamp after ip review

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-09-15 11:16:21 UTC
+# Last sync: 2026-09-15 12:26:27 UTC
