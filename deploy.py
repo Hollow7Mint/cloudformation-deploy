52537bafc44db9602a8bc390a3f8fa53d985f341
James Holloway <james.holloway94@gmail.com>
2026-09-15 06:34:08 +0000

    restore ecr scan result time for image audit

--- deploy.py
+++ deploy.py
@@ -231,4 +231,4 @@ class CloudFormationDeployer:
 if __name__ == "__main__":
     deployer = CloudFormationDeployer()
     deployer.deploy_all_stacks()
-# Last sync: 2026-09-15 06:07:33 UTC
+# Last sync: 2026-09-15 06:34:08 UTC
